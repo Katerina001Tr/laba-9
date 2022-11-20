0c5596f4dc76b441f5d4aee8486e7e638ad4c884
Katerina001Tr <93012937+Katerina001Tr@users.noreply.github.com>
2022-11-20 20:18:44 +0500

    добавление логирования и обработка ошибок для ввода числа N

--- Фт-210008_Терентьева_числа.py
+++ Фт-210008_Терентьева_числа.py
@@ -1,1 +1,17 @@
-
+import random
+import logging
+cc1 = 0 
+while cc1 != 1:  
+    try:
+        n = int(input('Компьютер загадал число: '))
+        logging.info('Ввод числа, которое загадал компьютер')
+        while n <= 0:  
+            print("Введите положительное число!")
+            logging.warning('Ввели число, меньшее единицы')
+            n = int(input('Введите положительное число, которое загадывает компьютер: '))
+    except ValueError:
+        print('Вы ввели не число!\nПробуйте снова\n')
+        logging.error('Ввели не число!')
+    else:
+        cc1 += 1
+        logging.info('Ввели корректное число({})'.format(n))
